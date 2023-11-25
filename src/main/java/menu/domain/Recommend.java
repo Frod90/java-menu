d6df16f1a36465e1df3fbaf677baf783f894d52e
Frod90 <142146516+Frod90@users.noreply.github.com>
2023-11-26 02:12:18 +0900

    Feat: 메뉴 추천 기능 구현

--- src/main/java/menu/domain/Recommend.java
+++ src/main/java/menu/domain/Recommend.java
@@ -22,12 +22,32 @@ public class Recommend {
 		}
 	}
 
+	public void generateCorrectMenu(String category) {
+
+		String menu = "";
+
+		while (menu.isBlank()) {
+
+			menu = RecommendMenuGenerator.generate(category);
+
+			if (menus.contains(menu)) {
+				menu = "";
+			}
+		}
+
+		menus.add(menu);
+	}
+
 	public String getUserName() {
 		return userName;
 	}
 
 	public List<String> getDislikeMenu() {
 		return dislikeMenu;
+	}
+
+	public List<String> getMenus() {
+		return menus;
 	}
 
 }
