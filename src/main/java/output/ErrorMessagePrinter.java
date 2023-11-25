c06a459d408bd7171a7a216813ad8792751e2da9
Frod90 <142146516+Frod90@users.noreply.github.com>
2023-11-25 22:08:32 +0900

    Feat: 오류 메세지 출력 기능 구현

---
+++ src/main/java/output/ErrorMessagePrinter.java
@@ -0,0 +1,28 @@
+package output;
+
+public class ErrorMessagePrinter {
+
+	private static final String ERROR_MESSAGE = "[ERROR] ";
+	private static final String INPUT_FORMAT_ERROR_MESSAGE = "구분자(,)를 이용해 나눠주세요";
+	private static final String USER_NAME_LENGTH_ERROR_MESSAGE = "코치의 이름은 최소 2글자 이상 최대 4글자 이하만 입력해야 합니다";
+	private static final String USER_NAME_MIN_SIZE_ERROR_MESSAGE = "코치는 최소 2명 이상 입력해야 합니다.";
+	private static final String USER_NAME_MAX_SIZE_ERROR_MESSAGE = "코치는 최소 5명 이하 입력해야 합니다.";
+
+	public static void printInputFormatError() {
+		System.out.println(ERROR_MESSAGE + INPUT_FORMAT_ERROR_MESSAGE);
+	}
+
+	public static void printUserNameLengthError() {
+		System.out.println(ERROR_MESSAGE + USER_NAME_LENGTH_ERROR_MESSAGE);
+	}
+
+	public static void printUserNameMinSizeError() {
+		System.out.println(ERROR_MESSAGE + USER_NAME_MIN_SIZE_ERROR_MESSAGE);
+	}
+
+	public static void printUserNameMaxSizeError() {
+		System.out.println(ERROR_MESSAGE + USER_NAME_MAX_SIZE_ERROR_MESSAGE);
+	}
+
+
+}
