71b28b6843f2347ae2f64d7ed2a55a252c84018e
Frod90 <142146516+Frod90@users.noreply.github.com>
2023-11-25 21:39:38 +0900

    Refactor: 테스트 -> 메인으로 이동

--- src/main/java/menu/input/InputMachine.java
+++ src/main/java/menu/input/InputMachine.java
@@ -2,7 +2,7 @@ package menu.input;
 
 import camp.nextstep.edu.missionutils.Console;
 
-public class inputMachine {
+public class InputMachine {
 
 	private static final String INPUT_USER_NAME_MESSAGE = "코치의 이름을 입력해 주세요. (, 로 구분)";
 	private static final String INPUT_DISLIKE_MENU_FORMAT = "%s(이)가 못 먹는 메뉴를 입력해 주세요.";
