8f927877e37295127eaf80f1044ea88a9a543aeb
Frod90 <142146516+Frod90@users.noreply.github.com>
2023-11-26 03:14:02 +0900

    Feat: 카테고리 목록에 해당 카테고리가 지정된 개수 이상 있으면 다시 카테고리를 생성하는 기능 추가 구현

--- src/main/java/menu/domain/CategoryGenerator.java
+++ src/main/java/menu/domain/CategoryGenerator.java
@@ -9,27 +9,42 @@ public class CategoryGenerator {
 
 	public static String generate(List<String> categorys) {
 
-		int categorySign = generateCategorySign();
-		return findCategory(categorySign);
+		long categoryCount;
+		String category;
+
+		do {
+			int categorySign = generateCategorySign();
+			category = findCategory(categorySign);
+			categoryCount = calculateCategoryCount(categorys, category);
+
+		} while (2 <= categoryCount);
+
+		return category;
+	}
+
+	public static long calculateCategoryCount(List<String> categorys, String category) {
+
+		return categorys.stream()
+			.filter(i -> i.equals(category))
+			.count();
 	}
 
 	private static int generateCategorySign() {
-		return Randoms.pickNumberInRange(1,5);
+		return Randoms.pickNumberInRange(1, 5);
 	}
 
 	private static String findCategory(int categorySign) {
 
 		String category = "";
 
-		for(DefaultMenu eachDefaultMenu : DefaultMenu.values()) {
+		for (DefaultMenu eachDefaultMenu : DefaultMenu.values()) {
 
-			if(eachDefaultMenu.getCategorySign() == categorySign) {
+			if (eachDefaultMenu.getCategorySign() == categorySign) {
 				category = eachDefaultMenu.getCategory();
 			}
 		}
 
 		return category;
 	}
-
 
 }
