3c07ea20f4e3423240c1e2cd57bcbe34ae1c1c5c
Frod90 <142146516+Frod90@users.noreply.github.com>
2023-11-25 21:23:57 +0900

    Feat: 사용자의 입력을 받는 기능 구현
    사용자의 이름과 못 먹는 음식을 입력 받는 기능 구현

---
+++ src/test/java/menu/input/inputMachine.java
@@ -0,0 +1,21 @@
+package menu.input;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class inputMachine {
+
+	private static final String INPUT_USER_NAME_MESSAGE = "코치의 이름을 입력해 주세요. (, 로 구분)";
+	private static final String INPUT_DISLIKE_MENU_FORMAT = "%s(이)가 못 먹는 메뉴를 입력해 주세요.";
+
+	public static String inputUserName() {
+		System.out.println();
+		System.out.println(INPUT_USER_NAME_MESSAGE);
+		return Console.readLine();
+	}
+
+	public static String inputDislikeMenu(String userName) {
+		System.out.println();
+		System.out.printf(INPUT_DISLIKE_MENU_FORMAT, userName);
+		return Console.readLine();
+	}
+}
