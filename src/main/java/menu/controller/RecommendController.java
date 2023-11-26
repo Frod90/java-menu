6c59c33bf5d3c2c66bf1cdf10d8f1567612288dd
Frod90 <142146516+Frod90@users.noreply.github.com>
2023-11-26 23:17:59 +0900

    Feat: 메뉴 추천 실행 기능 구현

--- src/main/java/menu/controller/RecommendController.java
+++ src/main/java/menu/controller/RecommendController.java
@@ -1,4 +1,56 @@
 package menu.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import menu.domain.CategoryGenerator;
+import menu.domain.RecommendMenu;
+import output.ResultPrinter;
+
 public class RecommendController {
+
+	List<String> userNames = new ArrayList<>();
+	List<String> categorys = new ArrayList<>();
+	List<RecommendMenu> recommendMenus = new ArrayList<>();
+
+	UserNameController userNameController = new UserNameController();
+	RecommendMenusMakeController recommendMenusMakeController = new RecommendMenusMakeController();
+
+	public void run() {
+		ResultPrinter.printStartMessage();
+
+		recommendWeekMenu();
+
+		ResultPrinter.printResultMessage();
+		ResultPrinter.printDay();
+		ResultPrinter.printCategory(categorys);
+
+		for (RecommendMenu eachRecommendMenu : recommendMenus) {
+			ResultPrinter.printRecommendMenu(eachRecommendMenu);
+		}
+
+		ResultPrinter.printEndMessage();
+	}
+
+	private void recommendWeekMenu() {
+
+		userNames = userNameController.makeCorrect();
+		recommendMenus = recommendMenusMakeController.makeCorrect(userNames);
+
+		for (int i = 0; i < 5; i++) {
+			conductDay();
+		}
+	}
+
+	private void conductDay() {
+
+		String category = CategoryGenerator.generate(categorys);
+		categorys.add(category);
+
+		for (RecommendMenu eachRecommendMenu : recommendMenus) {
+			eachRecommendMenu.generateCorrectMenu(category);
+		}
+
+	}
+
 }
